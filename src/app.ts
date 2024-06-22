c01acad746da97d3e3d91f284ce52cc41cb401e5
Kevin Garza <kevin_4569@msn.com>
2024-06-22 16:23:13 -0600

    se agrego metodos para pdf y ubicacion
    se agrego metodos para pdf y ubicacion

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,71 @@
-import "dotenv/config"
+import "dotenv/config";
 
-import { createBot, createProvider, createFlow, addKeyword, EVENTS } from '@builderbot/bot'
-import { MemoryDB as Database } from '@builderbot/bot'
-import { TwilioProvider  as Provider } from '@builderbot/provider-twilio'
-import { toAsk, httpInject } from "@builderbot-plugins/openai-assistants"
-import { typing } from "./utils/presence"
+import {
+  createBot,
+  createProvider,
+  createFlow,
+  addKeyword,
+  EVENTS,
+} from "@builderbot/bot";
+import { JsonFileDB  as Database } from "@builderbot/database-json";
+import { TwilioProvider as Provider } from "@builderbot/provider-twilio";
+import { toAsk, httpInject } from "@builderbot-plugins/openai-assistants";
+import { typing } from "./utils/presence";
 
-const PORT = process.env?.PORT ?? 3008
-const ASSISTANT_ID = process.env?.ASSISTANT_ID ?? ''
+const PORT = process.env?.PORT ?? 3008;
+const ASSISTANT_ID = process.env?.ASSISTANT_ID ?? "";
+let usuarioNuevo = true;
 
-const welcomeFlow = addKeyword<Provider, Database>(EVENTS.WELCOME)
-    .addAction(async (ctx, { flowDynamic, state, provider }) => {
-        await typing(ctx, provider)
-        const response = await toAsk(ASSISTANT_ID, ctx.body, state)
-       const chunks = response.split(/\n\n+/);
-for (const chunk of chunks) {
-    await flowDynamic([{ body: chunk.trim() }]);
-}
-    })
+const welcomeFlow = addKeyword<Provider, Database>(EVENTS.WELCOME).addAction(
+  async (ctx, { flowDynamic, state, provider }) => {
+    await typing(ctx, provider);
+    const response = await toAsk(ASSISTANT_ID, ctx.body, state);
+    const chunks = response.split(/\n\n+/);
+    for (const chunk of chunks) {
+      if (usuarioNuevo) {
+        await flowDynamic([
+          {
+            body: chunk.trim(),
+            media: "https://babelink.com.mx/images/Caroline.jpg",
+          },
+        ]);
+        usuarioNuevo = false;
+      } else {
+        await flowDynamic([{ body: chunk.trim() }]);
+      }
+    }
+  }
+);
+
+const locationFlow =  addKeyword(EVENTS.LOCATION)
+.addAnswer("He recibido tu ubicación muchas gracias", null, async (ctx) => {
+  const userLatitude = ctx.Latitude;
+  const userLongitude = ctx.Longitude;
+})
+
+const documentFlow = addKeyword(EVENTS.DOCUMENT)
+.addAnswer("Dame un momento para ver tu documento", null, async (ctx, { provider }) => {
+  const localPath = await provider.saveFile(ctx, {path:'tmp'})
+})
 
 const main = async () => {
-    const adapterFlow = createFlow([welcomeFlow])
-    const adapterProvider = createProvider(Provider, {
-        accountSid: process.env.ACC_SID, //AC4695aa720b4d700a***************
-        authToken: process.env.ACC_TOKEN, //3f6fae09f7a1c3534***************
-        vendorNumber: process.env.ACC_VENDOR, //+14155238886
-    })
-    const adapterDB = new Database()
+  const adapterFlow = createFlow([welcomeFlow, locationFlow, documentFlow]);
+  const adapterProvider = createProvider(Provider, {
+    accountSid: process.env.ACC_SID, //AC4695aa720b4d700a***************
+    authToken: process.env.ACC_TOKEN, //3f6fae09f7a1c3534***************
+    vendorNumber: process.env.ACC_VENDOR, //+14155238886
+  });
 
-    const { httpServer } = await createBot({
-        flow: adapterFlow,
-        provider: adapterProvider,
-        database: adapterDB,
-    })
+  const adapterDB = new Database({ filename: 'db.json' });
 
-    httpInject(adapterProvider.server)
-    httpServer(+PORT)
-}
+  const { httpServer } = await createBot({
+    flow: adapterFlow,
+    provider: adapterProvider,
+    database: adapterDB,
+  });
 
-main()
+  httpInject(adapterProvider.server);
+  httpServer(+PORT);
+};
+
+main();
